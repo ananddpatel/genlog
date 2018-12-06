403ec203e52fa8d00e9257433d3ca6dec292d5a6
Anand Patel <anandpatel3@hotmail.com>
2018-12-05 21:01:09 -0500

    temp

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import * as path from 'path';
-import * as mongoose from "mongoose";
+// import * as mongoose from "mongoose";
 import * as bodyParser from "body-parser";
 import * as morgan from "morgan";
 import * as express from 'express';
-
-const app = express();
+import { Server } from 'http';
+import * as socketIO from 'socket.io';
 
 import * as helloWorldController from './controllers/hello-world.controller';
+
+const app = express();
+const server = new Server(app);
+
+const io = socketIO(server);
+io.on('connect', client => {
+    console.log('client has connected');
+    
+    // setInterval(() => {
+    //     client.emit('genLogEvent2S', {hello: 'world from server'})
+    // }, 2000)
+    client.on('genLogEvent2S', data => { console.log('socket backend genLogEvent2S', data) });
+    client.on('genLogEvent', data => { console.log('socket backend genLogEvent', data) });
+    client.on('disconnect', () => { console.log('socket backend disconnect ') });
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,14 +33,14 @@ app.use('/public', express.static(path.join(__dirname, 'public/')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-mongoose.connect(
-    process.env.MONGO_URL as string,
-    { useNewUrlParser: true }
-  );
+// mongoose.connect(
+//     process.env.MONGO_URL as string,
+//     { useNewUrlParser: true }
+//   );
 
 app.get('/', helloWorldController.index);
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Started at http://localhost:${port}`);
 });
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,7 +1,23 @@
+
+
 const app = new Vue({
     el: '#app',
     data: {
         title: 'GENLog'
+    },
+    mounted() {
+        socket.on('connect', client => {
+            console.log('socket connected');
+            setInterval(() => {
+                client.emit('genLogEvent', {hello: 'world from client'})
+            }, 2000)
+        });
+        socket.on('genLogEvent2S', function(data){
+            console.log('socket genLogEvent2S', data);
+        });
+        socket.on('disconnect', function(){
+            console.log('socket disconnect');
+        });
     },
     computed: {
         
