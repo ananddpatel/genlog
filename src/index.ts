952bda2ba512b178622f89b6a2af1b64fae4c5af
Anand Patel <anandpatel3@hotmail.com>
2019-03-26 18:49:23 -0400

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 import * as path from 'path';
-import * as mongoose from "mongoose";
-import * as bodyParser from "body-parser";
-import * as morgan from "morgan";
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as morgan from 'morgan';
 import * as express from 'express';
 import { Server } from 'http';
 import * as socketIO from 'socket.io';
+import * as cors from 'cors';
 
 import * as helloWorldController from './controllers/hello-world.controller';
 
@@ -17,34 +18,33 @@ let socketClient;
 
 const io = socketIO(server);
 io.on('connect', client => {
-    if (!socketClient) {
-        socketClient = client
-        console.log('connected');
-    }
+  if (!socketClient) {
+    socketClient = client;
+    console.log('connected');
+  }
 });
 
-export const emitLogEvent = (logData) => {
-    io.sockets.emit('logEvent', logData);
-}
+export const emitLogEvent = logData => {
+  io.sockets.emit('logEvent', logData);
+};
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev")); // logger
+app.use(morgan('dev')); // logger
 
 app.use('/public', express.static(path.join(__dirname, 'public/')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-mongoose.connect(
-    process.env.MONGO_URL as string,
-    { useNewUrlParser: true }
-  );
+mongoose.connect(process.env.MONGO_URL as string, { useNewUrlParser: true });
 
 app.get('/', helloWorldController.index);
 app.post('/log', helloWorldController.emitLog);
 app.get('/logs', helloWorldController.getLogs);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log(`Started at http://localhost:${port}`);
+  console.log(`Started at http://localhost:${port}`);
 });
